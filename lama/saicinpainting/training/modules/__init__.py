4df3821f2e2b5e10dc359d98c077e6e57dcce028
datnvt <nguyenvanthanhdat1810@gmail.com>
2024-04-26 11:20:25 +0700

    chore:

--- lama/saicinpainting/training/modules/__init__.py
+++ lama/saicinpainting/training/modules/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
-from saicinpainting.training.modules.ffc import FFCResNetGenerator
-from saicinpainting.training.modules.pix2pixhd import GlobalGenerator, MultiDilatedGlobalGenerator, \
+from Inpaint_Anything.lama.saicinpainting.training.modules.ffc import FFCResNetGenerator
+from Inpaint_Anything.lama.saicinpainting.training.modules.pix2pixhd import GlobalGenerator, MultiDilatedGlobalGenerator, \
     NLayerDiscriminator, MultidilatedNLayerDiscriminator
 
 def make_generator(config, kind, **kwargs):
